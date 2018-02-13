2ce81ba694a3e8739212db1adeb5ae2e18a0b297
Dante Planterose <dplanterose@gmail.com>
2018-02-13 11:55:10 +0100

    ok

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  before_create :set_completed_to_false
+  private
+  def set_completed_to_false
+    self.completed = false unless self.completed == true
+  end
 end
